5fa2491ec65855c4d448c40c4fcea64c7734b6dd
Jorge-Sorto <97974448+Jorge-Sorto@users.noreply.github.com>
2022-02-20 22:34:40 -0600

    Create ConvertirMonedas.vue

---
+++ ConvertirMonedas.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <br>
+        <br>
+        <h2>Dolares a Euros</h2>
+        <input v-model="vEntrada" type="numbre" placeholder="Ingrese la cantidad de dolares"/>
+        <br>
+        <b-button variant="success" v-on:click="Calcularmoneda" title="Convertir">Convertir</b-button>
+        <br>
+        <h4>{{vEntrada}} dolares equivalen a: {{resultado}} euros</h4>
+
+    </div>
+</template>
+<script>
+export default{
+    name: 'ConvertirMonedas',
+    data(){
+        return{
+            vEntrada: '',
+            resultado: ''
+        }
+    },
+    methods:{
+        Calcularmoneda(){
+            this.resultado = this.vEntrada * 0.88;
+            console.log(this.vEntrada);
+            console.log(this.resultado)
+        }
+    }
+}
+</script>
