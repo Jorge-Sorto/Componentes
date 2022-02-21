7ae058d7c1358d940404261ce234c7e28590a662
Jorge-Sorto <97974448+Jorge-Sorto@users.noreply.github.com>
2022-02-20 22:34:10 -0600

    Create ConvertirLongitud.vue

---
+++ ConvertirLongitud.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <br>
+        <br>
+        <h2>Metros a Centimetros</h2>
+        <input v-model="vEntrada" type="numbre" placeholder="Ingrese los metros a convertir"/>
+        <br>
+        <b-button variant="success" v-on:click="Calcularlongitud" title="Convertir">Convertir</b-button>
+        <br>
+        <h4>{{vEntrada}} metros equivalen a: {{resultado}} centimetros</h4>
+
+    </div>
+</template>
+<script>
+export default{
+    name: 'ConvertirLongitud',
+    data(){
+        return{
+            vEntrada: '',
+            resultado: ''
+        }
+    },
+    methods:{
+        Calcularlongitud(){
+            this.resultado = this.vEntrada * 100;
+            console.log(this.vEntrada);
+            console.log(this.resultado)
+        }
+    }
+}
+</script>
