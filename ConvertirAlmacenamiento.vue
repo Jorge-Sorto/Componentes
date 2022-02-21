518406ca90944ad66550bbbb9e0a0008ce385450
Jorge-Sorto <97974448+Jorge-Sorto@users.noreply.github.com>
2022-02-20 22:35:11 -0600

    Create ConvertirAlmacenamiento.vue

---
+++ ConvertirAlmacenamiento.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <br>
+        <br>
+        <h2>Megabytes a Gigabytes</h2>
+        <input v-model="vEntrada" type="numbre" placeholder="Ingrese el numero de megas a convertir"/>
+        <br>
+        <b-button variant="success" v-on:click="Calcularalmacenamiento" title="Convertir">Convertir</b-button>
+        <br>
+        <h4>{{vEntrada}} megabytes equivalen a: {{resultado}} gigabytes</h4>
+
+    </div>
+</template>
+<script>
+export default{
+    name: 'ConvertirAlmacenamiento',
+    data(){
+        return{
+            vEntrada: '',
+            resultado: ''
+        }
+    },
+    methods:{
+        Calcularalmacenamiento(){
+            this.resultado = this.vEntrada * 0.001;
+            console.log(this.vEntrada);
+            console.log(this.resultado)
+        }
+    }
+}
+</script>
